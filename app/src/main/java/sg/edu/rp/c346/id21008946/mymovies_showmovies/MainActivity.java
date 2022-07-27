fe95011764b3beae2f7707d4bcd04d13d468511d
sleepz1312 <21024625@myrp.edu.sg>
2022-07-27 12:19:31 +0800

    activity_main.xml changed MainActivity.java changed activity_modify_page.xml changed

--- app/src/main/java/sg/edu/rp/c346/id21008946/mymovies_showmovies/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21008946/mymovies_showmovies/MainActivity.java
@@ -3,12 +3,39 @@ package sg.edu.rp.c346.id21008946.mymovies_showmovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    CustomAdapter caMovie;
+    ArrayList<Movies> alMovieList;
+    TextView tvTitle, tvGenre, tvYear, tvRating;
+    EditText etTitle, etGenre, etYear;
+    Spinner spnRating;
+    Button btnInsert, btnShowList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTitle.findViewById(R.id.tvTitle);
+        tvGenre.findViewById(R.id.tvGenre);
+        tvYear.findViewById(R.id.tvYear);
+        tvRating.findViewById(R.id.tvRating);
+        etTitle.findViewById(R.id.etTitle);
+        etGenre.findViewById(R.id.etGenre);
+        etYear.findViewById(R.id.etYear);
+        spnRating.findViewById(R.id.spnRating);
+        btnInsert.findViewById(R.id.btnInsert);
+        btnShowList.findViewById(R.id.btnShowList);
+
+
     }
 }
